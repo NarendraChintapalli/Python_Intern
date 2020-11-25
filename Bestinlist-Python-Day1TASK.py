de99f77853fdc98d541e8873ed7165dd536ee6a1
NarendraChintapalli <75028874+NarendraChintapalli@users.noreply.github.com>
2020-11-25 19:59:12 +0530

    Add files via upload

---
+++ Bestinlist-Python-Day1TASK.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+str="Best"
+print(str+"Enlist")
+
+
+# In[2]:
+
+
+print(1+9)
+
+
+# In[3]:
+
+
+print(1-9)
+
+
+# In[4]:
+
+
+a=1
+b=2
+c=a+b
+print(c)
+
+
+# In[5]:
+
+
+print("30 days 30 hours challenge")
+
+
+# In[6]:
+
+
+Hours = "thirty"
+print(Hours)
+
+
+# In[11]:
+
+
+Days ="Thirty Days"
+print(Days[5])
+
+
+# In[12]:
+
+
+challenge = "I will win"
+print(challenge[7:10])
+
+
+# In[13]:
+
+
+Challenge ="I will win"
+print(len(Challenge))
+
+
+# In[14]:
+
+
+Challenge ="I will win"
+print(Challenge.lower())
+
+
+# In[15]:
+
+
+a = "30 days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+
+# In[16]:
+
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+
+# In[17]:
+
+
+from platform import python_version
+
+
+# In[18]:
+
+
+print(python_version())
+
+
+# In[19]:
+
+
+s="Hey Iam Sowmya"
+s[1:]
+
+
+# In[20]:
+
+
+s="This is my 1st Intern"
+s[6:]
+
+
+# In[ ]:
+
+
+
+
