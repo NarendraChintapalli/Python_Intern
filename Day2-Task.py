344785a8f99c7b2a7708d96c0c6de624b1aec37f
NarendraChintapalli <75028874+NarendraChintapalli@users.noreply.github.com>
2020-11-26 21:26:55 +0530

    Add files via upload

---
+++ Day2-Task.py
@@ -0,0 +1,48 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Creating three Variables
+>>> a=50
+>>> b=50
+>>> c=50
+>>> print(a/10)
+5.0
+>>> print(b*50)
+2500
+>>> print(c+60)
+110
+>>> a=b=c=80
+>>> print(a/10)
+8.0
+>>> print(b*50)
+4000
+>>> print(c+60)
+140
+>>> 
+>>> 
+>>> 
+>>> #Replacing character
+>>> A="DHONI"
+>>> print(A.replace("O","G"))
+DHGNI
+>>> B="SAMCURRAN"
+>>> print(B.replace("M","G"))
+SAGCURRAN
+>>> 
+>>> 
+>>> 
+>>> #Conversion of int,float datatypes
+>>> a=100
+>>> b=50.6
+>>> float(a)
+100.0
+>>> int(b)
+50
+>>> c=50
+>>> d=60.3333
+>>> float(a)
+100.0
+>>> int(d)
+60
+>>> float(c)
+50.0
+>>> 
