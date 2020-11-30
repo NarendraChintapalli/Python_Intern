0d5832f54826413056a472fae9c579f2adb3711d
NarendraChintapalli <75028874+NarendraChintapalli@users.noreply.github.com>
2020-11-30 12:55:22 +0530

    Add files via upload

---
+++ Day5-Task.py
@@ -0,0 +1,55 @@
+#Addition of two numbers is +value
+def add(x,y):
+    return(x+y)
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=add(a,b)
+print("Addition of",a,"and",b,"is",c)
+
+
+
+#Subtraction of two numbers is -value
+def sub(x,y):
+    return(x,y)
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=sub(a,b)
+print("Subtraction of",a,"and",b,"is",c)
+
+
+
+#Division of two numbers is +value
+def division(x,y):
+    return(x,y)
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=division(a,b)
+print("Division of",a,"and",b,"is",c)
+
+
+
+#Multiplication of two numbers is +value
+def multiplication(x,y):
+    return(x,y)
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=multiplication(a,b)
+print("Multiplication of",a,"and",b,"is",c)
+
+
+
+#COVID
+def covid(name,temperature=98):
+    print("Name of the patient:",name)
+    print("Body Temperature of the patient:",temperature)
+
+covid("Ramya",98)
+covid("Sowmya",101)
+covid("Divya",102)
+covid("Koushi",103)
+
+
+
+
+ 
+
